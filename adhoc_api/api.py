5a01f88eddedf6d71c1e24328cfcd924066f588d
Brandon Kolze <james.b.kolze@usace.army.mil>
2025-07-11 12:40:40 -0400

    Add endpoint for retrieving scripts catalogs

--- adhoc_api/api.py
+++ adhoc_api/api.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from .auth import get_current_user, User
+from .catalog import get_scripts_catalog
 from .job_runner.local import LocalJobRunner
 
 runner = LocalJobRunner()
@@ -9,3 +12,13 @@ router = APIRouter()
 @router.get("/")
 async def test():
     return runner.run_job("lrh", "test.py")
+
+
+@router.get("/scripts/catalog")
+async def get_user_scripts_catalog(user: User = Depends(get_current_user)):
+    all_scripts = {}
+    for office in user.offices:
+        office_scripts = get_scripts_catalog(office)
+        if office_scripts:
+            all_scripts[office] = office_scripts
+    return all_scripts
