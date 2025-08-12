8c889c938b9691377b42106a69a7d5ec11f17004
Brandon Kolze <james.b.kolze@usace.army.mil>
2025-08-12 13:57:08 -0400

    Use TanStack Router for client-side routing

---
+++ ui/src/routes/__root.tsx
@@ -0,0 +1,18 @@
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { SiteWrapper, Container } from "@usace/groundwork";
+import "@usace/groundwork/dist/style.css";
+import AuthButton from "../features/auth/AuthButton";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <SiteWrapper navRight={<AuthButton />}>
+      <Container>
+        <Outlet />
+      </Container>
+    </SiteWrapper>
+  );
+}
