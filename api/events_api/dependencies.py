38e9a6118c86c7c56903750fb97d83030f46cd6e
Brandon Kolze <james.b.kolze@usace.army.mil>
2025-08-20 13:40:19 -0400

    Shorten dynamo job db package name

--- api/events_api/dependencies.py
+++ api/events_api/dependencies.py
@@ -2,7 +2,7 @@ import os
 
 from .auth.user import get_current_user_keycloak, get_current_user_mock
 from .job_database.base import JobDatabase
-from .job_database.dynamo_job_database import DynamoJobDatabase
+from .job_database.dynamo import DynamoJobDatabase
 
 MOCK_USER = os.getenv("MOCK_USER", "false")
 if MOCK_USER == "true":
