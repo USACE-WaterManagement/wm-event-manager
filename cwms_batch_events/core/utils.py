bb2272e6f36b242db86b384a266c6e876c6b7e7c
Brandon Kolze <james.b.kolze@usace.army.mil>
2026-01-20 13:40:13 -0500

    Add OFFICES constant to link office and division

--- cwms_batch_events/core/utils.py
+++ cwms_batch_events/core/utils.py
@@ -86,3 +86,15 @@ ALL_OFFICES = [
     "UNK",
     "ERD",
 ]
+
+# TODO: Add all offices
+OFFICES = {
+    "lrc": {"office-group": "lrd"},
+    "lre": {"office-group": "lrd"},
+    "lrh": {"office-group": "lrd"},
+    "lrl": {"office-group": "lrd"},
+    "lrn": {"office-group": "lrd"},
+    "lrp": {"office-group": "lrd"},
+    "swt": {"office-group": "swd"},
+    "spk": {"office-group": "spd"},
+}
