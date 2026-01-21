9f6d906d046089d0997881ce4456fcd28c248efe
Brandon Kolze <james.b.kolze@usace.army.mil>
2026-01-21 08:45:17 -0500

    Remove job_runner dependency from API
    Dispatching to specific job_runners has been decoupled from the API. The
    API only submits a JobMessage to the messaging queue.

--- cwms_batch_events/api/dependencies.py
+++ cwms_batch_events/api/dependencies.py
@@ -10,8 +10,6 @@ from cwms_batch_events.core.job_database.postgres.session import create_session
 from cwms_batch_events.core.job_logger.base import JobLogger
 from cwms_batch_events.core.job_logger.cloudwatch import CloudWatchJobLogger
 from cwms_batch_events.core.job_logger.s3 import S3JobLogger
-from cwms_batch_events.core.job_runner.base import JobRunner
-from cwms_batch_events.core.job_runner.local import LocalJobRunner
 from cwms_batch_events.core.queue import JobQueue
 from cwms_batch_events.core.settings import settings
 
@@ -42,10 +40,3 @@ def get_job_logger(db=Depends(get_db_session)) -> JobLogger:
 
 def get_job_queue() -> JobQueue:
     return JobQueue()
-
-
-def get_job_runner(
-    db: JobDatabase = Depends(get_job_database),
-    logger: JobLogger = Depends(get_job_logger),
-) -> JobRunner:
-    return LocalJobRunner(db=db, logger=logger)
