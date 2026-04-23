3bcce3148c7f8b08b0eff67bf4b84ff7fd7cfa5d
Eli_Litzelman <eli.j.litzelman@usace.army.mil>
2026-04-23 10:59:18 -0400

    Changed LoadingSpinner component to remove unnecessary  div

--- ui/src/shared/components/LoadingSpinner.tsx
+++ ui/src/shared/components/LoadingSpinner.tsx
@@ -1,9 +1,7 @@
 import { AiOutlineLoading } from "react-icons/ai";
 
 const LoadingSpinner = () => (
-  <div className="inline-flex items-center gap-2">
     <AiOutlineLoading className="animate-spin text-xl" />
-  </div>
 );
 
 export default LoadingSpinner;
